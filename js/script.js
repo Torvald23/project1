0d486dd0c416f0e433f00d94458b30dbcd3635bc
Gleb <torvaldgleb@gmail.com>
2022-06-14 20:47:42 +0300

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+let iner = 10,
+  decr = 10;
+
+// ++iner;
+// --decr;
+
+console.log(++iner);
+console.log(iner--);
+console.log(iner);
+
+console.log(5 % 3);
+
+console.log(2 + 2 * 2 = 6);
+
+const isChecked = false,
+  isclose = false;
+console.log(isChecked || !isclose);
